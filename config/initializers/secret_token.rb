465d2b42f51b642ed3bee2aa8ff798f537296815
Uchenna Okafor <ramzauchenna@gmail.com>
2014-09-21 18:25:08 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tava::Application.config.secret_key_base = '45a4548f89851af8fd7a26e6c3fb75349d1040cf064767b70ad24dd1b0c12a6aab4fc099d0dd73f3327e549e7312d46efc55a39898476eb4724b57cfe883457d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tava::Application.config.session_store :cookie_store, key: '_tava_session'
