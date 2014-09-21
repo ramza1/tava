ed251a05e058517b9ec12a946eaa06dc1dbc5651
Uchenna Okafor <ramzauchenna@gmail.com>
2014-09-21 18:45:09 +0100

    j

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ require "bundler/capistrano"
 
 server "208.68.37.172", :web, :app, :db, primary: true
 
-set :application, "tave"
+set :application, "tava"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
@@ -40,6 +40,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
+
   after "deploy:finalize_update", "deploy:symlink_config"
 
 
